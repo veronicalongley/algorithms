063d3d52ba18787eda2d2302200273acc72f6cf0
veronicalongley <veronica.longley1@marist.edu>
2021-10-06 12:39:40 -0400

    main create and file read into array

---
+++ assignment2/src/assignment2/main.java
@@ -0,0 +1,67 @@
+package assignment2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main 
+{
+
+static Scanner keyboard = new Scanner(System.in);
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		//initialize variables used to read in items from list
+		String fileName = ""; 
+		final int NUMOFITEMS = 666;
+		String [] magicItems = new String [NUMOFITEMS];
+		String theitem = null;
+		fileName = "magicitems";
+		
+		//input item names from file and store in array
+		try
+			{
+			Scanner readFile = new Scanner (new File (fileName));
+			int i = 0; 
+			while(readFile.hasNextLine())
+			{
+				theitem = readFile.nextLine();
+				magicItems[i] = theitem;
+				i++;
+			}//while
+			readFile.close();
+			}//try
+		catch(FileNotFoundException ex)
+			{
+			System.out.println("Failed to find file: "+ fileName);
+			}//catch
+		catch(InputMismatchException ex)
+			{
+			System.out.println("Type mismatch for the number I tried to read. ");
+			System.out.println(ex.getMessage());
+			}//catch
+		catch(NumberFormatException ex)
+			{
+			System.out.println("Failed to convert String text into an integer value. ");
+			System.out.println(ex.getMessage());
+			}//catch
+		catch(NullPointerException ex)
+			{
+			System.out.println("Null pointer exception. ");
+			System.out.println(ex.getMessage());
+			}//catch
+		catch(Exception ex)
+			{
+			System.out.println("Oops, something went wrong. ");
+			ex.printStackTrace();
+			}//catch
+		
+		System.out.println(magicItems[665]);
+		
+	}//main
+
+}//main
