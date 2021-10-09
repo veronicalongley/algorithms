c9e2e1791f111f36a17bb32503963ede38518ea5
veronicalongley <veronica.longley1@marist.edu>
2021-10-08 21:39:20 -0400

    comments added

--- assignment2/src/assignment2/main.java
+++ assignment2/src/assignment2/main.java
@@ -93,14 +93,22 @@ static Scanner keyboard = new Scanner(System.in);
 static int selectionSort( String items[], final int TOTNUM)
 {
 	int comparisons = 0; 
+	//for loop to go through list and hold space for lowest
+	//value to be swapped to 
 	for(int i = 0; i < TOTNUM - 1; i++)
 	{
 		int minIndex = i; 
 		String minItem = items[i];
 		String temp = "";
+		
+		//for loop to compare the rest of the element in the 
+		//array with the value from the for i loop 
 		for (int j = i + 1; j < TOTNUM; j++)
 		{
 			comparisons ++;
+			//if items[j] is less than the current minimum
+			//item in the list assign the minimum index to 
+			// be j
 			if (items[j].compareToIgnoreCase(minItem) < 0)
 			{
 				
@@ -109,6 +117,7 @@ static int selectionSort( String items[], final int TOTNUM)
 			}//if
 		}//for j
 
+		//swap if not already in correct index
 		if (minIndex != i)
 		{	temp = items[minIndex];
 	        items[minIndex] = items[i];
@@ -135,16 +144,24 @@ static void shuffle (String items [], final int TOTNUM)
 static int insertion (String items[],final int TOTNUM ) 
 {
 	int comparisons = 0;
+	//for loop to go through array from beginning to end 
 	for(int i = 1; i < TOTNUM ; i++)
 	{
 		String key = items[i];
 		int j = i -1; 
+		
+		//while the item being added is greater than the end of the 
+		//already sorted items and j has not reached the start of 
+		//the array we slide values until j has found its position
 		while((j > -1) && ( items[j] .compareToIgnoreCase(key) >0 ))
 		{
 			comparisons ++; 
+			//slide affected values to new location
 			items [j+1] = items [j];  
              j--; 
 		}//for j
+		
+		//increment the key
 		items[j+1] = key; 
 	}//for i
 	return comparisons;
@@ -154,10 +171,14 @@ static int insertion (String items[],final int TOTNUM )
 static void mergeSort (String items[])
 {
 
+	//if the sub-lists are not yet legth one
 	if(items.length > 1)
 	{
+		//divide in half again 
 		String[] left = new String [items.length/2];
 		String[] right = new String [items.length- (items.length /2)];
+		
+		//copy items into new arrays
 		for(int i = 0; i< left.length; i++)
 		{
 			left[i]= items[i];
@@ -167,6 +188,7 @@ static void mergeSort (String items[])
 			right[i]= items[i+items.length/2];
 		}//for i 
 		
+		//recursively calls merge sort for each side 
 		mergeSort(left);
 		mergeSort(right);
 		merge(items, left, right);
@@ -177,6 +199,7 @@ static void mergeSort (String items[])
 
 static void merge(String[]items, String[]left, String[]right)
 {
+	//merges sub-arrays
 	int x = 0;
 	int y = 0;
 	
@@ -206,38 +229,48 @@ static void random(String items [], int left, int right)
 	items[right] = temp;
 }//random
 
-static int pivot(String items[], int left, int right)
+static int partition(String items[], int left, int right)
 {
 	random(items, left, right);
 	String pivot = items[right];
+	
+	//indicates elements smaller than the pivot
 	int x = (left - 1);
 	for(int j = left; j < right; j++)
 	{
 		quickComparisons++;
+		//if current position is smaller than pivot
 		if(items[j].compareToIgnoreCase(pivot) < 0)
 		{
+			//increase the number of elements smaller than pivot
 			x++;
+			
+			//swap
 			String temp = items[x];
             items[x] = items[j];
             items[j] = temp;
-       //     quickComparisons++;
+
 		}//if
 		
 	}//for
 	
+	//swap remaining upper elements
 	String temp2 = items[x+1];
     items[x+1] = items[right];
     items[right] = temp2;
 
     return x+1;
-}//pivot
+}//partition
 
 static void quicksort (String items[], int left, int right)
 {
 	 
 	if(left < right)
 	{
-		int piv = pivot(items, left, right);
+		//index of pivot
+		int piv = partition(items, left, right);
+		//sort elements above and below the pivot
+		//recursion
 		quicksort(items, left, piv-1);
 		quicksort(items, piv+1, right);
 		
